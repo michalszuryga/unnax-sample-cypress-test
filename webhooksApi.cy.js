618036aab17cc75e5193ee9874c0ceff0707a919
Michal Szuryga <michal.szuryga@ihsmarkit.com>
2024-07-18 00:14:13 +0200

    Initial commit with sample API test

---
+++ webhooksApi.cy.js
@@ -0,0 +1,43 @@
+describe("Webhooks API", () => {
+  it("Webhooks Create - should create a new webhook successfully", () => {
+    const apiUrl =
+      "https://stoplight.io/mocks/unnax/payments/38328916/api/v3/webhooks/";
+    const authToken = "Unnax f23ftbzfasdh";
+
+    cy.request({
+      method: "POST",
+      url: apiUrl,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: authToken,
+      },
+      body: {
+        client: "email",
+        event: "event_payment_transfer_auto_processed",
+        target: "test@test.com",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+
+      cy.wrap(response.body.id).as("webhookId");
+    });
+
+    cy.get("@webhookId").then((webhookId) => {
+      cy.request({
+        method: "GET",
+        url:`${apiUrl}3`,
+        //url: `${apiUrl}${webhookId}`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: authToken,
+        },
+      }).then((getResponse) => {
+        expect(getResponse.status).to.eq(200);
+        cy.log('GET Response:', getResponse.body.event);
+        expect(getResponse.body).to.have.property("id", 3);//webhookId);
+        expect(getResponse.body.event).to.eq("fitnance_read");
+        //expect(getResponse.body.event).to.include("event_payment_transfer_auto_processed")
+      });
+    });
+  });
+});
